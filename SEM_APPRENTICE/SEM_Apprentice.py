4bfd56034ed1b9a5f126a5cde8324009c52cfae9
notwopr <21273266+notwopr@users.noreply.github.com>
2023-03-23 00:21:04 -0700

    fixed listener,timestamp, more

--- SEM_APPRENTICE/SEM_Apprentice.py
+++ SEM_APPRENTICE/SEM_Apprentice.py
@@ -7,6 +7,7 @@ WELCOME TO SEM_APPRENTICE!
 """
 # SYSTEM IMPORTS
 import os
+import re
 from pathlib import Path
 import logging
 from tkinter import messagebox
@@ -14,17 +15,19 @@ import tkinter as tk
 import pyautogui
 import cv2
 import numpy as np
-from pynput.mouse import Listener
-
+from pynput import mouse
+from pynput import keyboard
 
 # get the current directory path
+#__file__= r'C:\Users\...'
+# username = os.getlogin()
+
+# a = r'C:\Users'
+# b = r'Documents'
+# __file__= a + '\\'+ username +'\\' +b
+
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-"""DO NOT MODIFY CODE BELOW THIS LINE"""
-"""DO NOT MODIFY CODE BELOW THIS LINE"""
-"""DO NOT MODIFY CODE BELOW THIS LINE"""
-
 
 class DirNames:
     DN_DATA_PARENT = 'SEMBOT_DATA'
@@ -39,7 +42,6 @@ class FullPathElements:
     F1_LOGS = F0_DATA_PARENT + [DirNames.DN_LOGS]
     F1_SCREENSHOTS = F0_DATA_PARENT + [DirNames.DN_SCREENSHOTS]
 
-
 class PathOperations:
     
     # create one pathstring from list of path elements with '/' or '\' as separator
@@ -53,7 +55,6 @@ class PathOperations:
     def create_path_string(self, list_of_path_elements):
         return self.create_path_string_custom(list_of_path_elements, '/')
     
-
 class MachineOperations:
 
     def check_if_directory_is_directory(self, path_to_dir: str):
@@ -72,7 +73,6 @@ class MachineOperations:
         else:
             print(f"Directory {path_to_dir} already exists.")
 
-
 def build_directories():
     
     # get sorted list of all SEMBot Directories to build
@@ -89,57 +89,113 @@ def build_directories():
         pathstring = PathOperations().create_path_string(getattr(f, d))
         MachineOperations().create_nonexistent_directory(pathstring)
 
+def get_key_symbol(key):
+    """given key, determines the name of that key, regardless whether it has .char, .vk or neither attribute"""
+    if hasattr(key, 'vk') and 96 <= key.vk <= 105:
+        return key.vk - 96
+    if hasattr(key, 'char'):
+        return key.char
+    else:
+        return key
+
+def snap_and_save(signal, detail, mode):
+    # compose log message
+    match mode:
+        case 'keyboard':
+            message = f'{signal} {get_key_symbol(detail)}'
+        case 'mouse':
+            message = f"{signal} {detail}"
+
+    # log message
+    logging.info(message)
+
+    log_message = formatter.log_message
+
+    # format filename
+    raw_timestamp = formatter.timestamp
+    timestamp_formatted = re.sub(r'\,|\:|\-|\s', '', raw_timestamp)
+    filename = f"{timestamp_formatted} {message}.png"
+
+    # takes snapshot and save image to disk with that filename
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)  #why do I need this step # also can I save just the template # can I save only the program window, not the entire screenshot
+    ss_path = PathOperations().create_path_string(FullPathElements.F1_SCREENSHOTS+[filename])
+    cv2.imwrite(ss_path, image)
+
+def on_press(key):
+    global lock
+    if not lock: 
+        # if lock is True, that means Stop Recording? prompt is open, so do not log until prompt closes
+        snap_and_save('pressed', key, 'keyboard')
+
+def on_release(key):
+    global lock
+    if not lock: 
+        # if lock is True, that means Stop Recording? prompt is open, so do not log until prompt closes
+        snap_and_save('released', key, 'keyboard')
+
+def on_move(x, y):
+    global lock   
+    if not lock:  
+        # if lock is True, that means Stop Recording? prompt is open, so do not log until prompt closes
+        logging.info(f"moved ({x}, {y})")  # coordinates are what mouse moved TO (according to pynput docs)
+
+def on_click(x, y, button, pressed):
+    global lock   
+    if not lock and pressed:  
+        # if lock is True, that means Stop Recording? prompt is open, so do not log until prompt closes
+        snap_and_save('clicked', f'({x}, {y}) {button}', 'mouse')
+
+def on_scroll(x, y, dx, dy):
+    global lock
+    if not lock: 
+        # if lock is True, that means Stop Recording? prompt is open, so do not log until prompt closes
+        logging.info(f"scrolled ({x}, {y})({dx}, {dy})")
+
+    # If User is finished recording, user moves mouse to upperleft corner and scrolls
+    if x<2 and y<2 and not lock:
+    # if lock is False, set to True and display message, suspend Listener
+        lock = True
+        # Display confirmation box
+        stop_record = UIOperations().yesno('STOP RECORDING?', 'Are you sure you want to stop recording?')
+        if stop_record:
+            return False
+        lock = False  # if chose 'No', then change lock to False and continue logging
+
+
+class UIOperations:
+    def yesno(self, title, question):
+        """Gives a Yes/No popup dialog box that stays on top of everything else on the screen always and doesn't close until you click yes or no"""
+        root = tk.Tk()
+        root.withdraw()
+        root.focus_set() # Set the messagebox as the top window
+        root.attributes('-topmost', True)  # keep window on top of others
+        response = messagebox.askyesno(title, question)
+        root.destroy()  # destroy the root window
+        return response
+
+# Create a custom Formatter that captures the entire log message in a variable
+class MyFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+        self.log_message = None
+        self.timestamp = None
+
+    def format(self, record):
+        self.log_message = super().format(record)
+        self.timestamp = self.formatTime(record, self.datefmt)
+        return super().format(record)
 
 
 
+    
 # global variable to prevent multiple message boxes from being displayed at once
 lock = False  
 
 
-def on_move(x, y):
-    logging.info(f"Mouse moved to ({x}, {y})")
-
-def on_click(x, y, button, pressed):
-    if pressed:
-        logging.info(f"Mouse clicked at ({x}, {y}) with {button}")
-
-        image = pyautogui.screenshot()
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)  #why do I need this step # also can I save just the template # can I save only the program window, not the entire screenshot
-        ss_uncropped_filename = f"Mouse clicked at ({x}, {y}) with {button}.png"
-        path_ss_uncropped = PathOperations().create_path_string(FullPathElements.F1_SCREENSHOTS+[ss_uncropped_filename])
-        cv2.imwrite(path_ss_uncropped, image)
-
-def on_scroll(x, y, dx, dy):
-
-    logging.info(f"Mouse scrolled at ({x}, {y})({dx}, {dy})")
-
-    # If User is finished recording, user moves mouse to upperleft corner and scrolls
-    if x<2 and y<2:
-
-        global lock   
-        if not lock:  # if lock is False, set to True and display message to suspend Listener
-            lock = True
-            
-            # c. Display confirmation box
-            root = tk.Tk()
-            root.withdraw()
-            root.focus_set() # Set the messagebox as the top window
-            root.attributes('-topmost', True)  # keep window on top of others
-            stop_record = messagebox.askyesno('STOP RECORDING?', 'Are you sure you want to stop recording?')
-            root.destroy() # destroy the root window
-
-            if stop_record:
-                return False
-            lock = False  # lock is True, don't display message box
-
-
+"""EXECUTABLES BELOW"""
 # 1. Get User Confirmation to Begin Recording
-root = tk.Tk()
-root.withdraw()
-root.focus_set() # Set the messagebox as the top window
-root.attributes('-topmost', True)  # keep window on top of others
-start_record = messagebox.askyesno('START RECORDING?', 'Are you sure you want to start recording?')
-root.destroy()  # destroy the root window
+start_record = UIOperations().yesno('START RECORDING?', 'Hello!  Should I start recording?')
 
 # If user confirms to begin recording...
 if start_record:
@@ -149,11 +205,24 @@ if start_record:
 
     # 3. Activate Logger
     path_to_logfile = PathOperations().create_path_string(FullPathElements.F1_LOGS + [FileNames().FN_LOG])
-    logging.basicConfig(filename=path_to_logfile, level=logging.DEBUG, format='%(asctime)s: %(message)s', force =True)
+    
+    # Set up logging to a file using the custom Formatter to later store created log message to variable 
+    formatter = MyFormatter(fmt='%(asctime)s: %(message)s')
+    handler = logging.FileHandler(filename=path_to_logfile, mode='a')  # mode 'a' means append new log messages; mode 'w' means 'write' rather rewrite new log messages (previous messages are overwritten)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    # Log start of session
+    logging.info(f"SEM Apprentice activated!")
 
     # 4. Activate Listener
-    with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
-        listener.join()
-
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as k_listener, \
+        mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as m_listener:
+        m_listener.join()
+        if lock is True:
+            logging.info(f"SEM Apprentice terminated.")
+            exit()
 else:
     exit()
